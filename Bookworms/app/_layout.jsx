9824c42c0a368d9439e8d53377e4019aa1f2fe2a
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-07-14 16:42:29 -0400

    layout setup

--- Bookworms/app/_layout.jsx
+++ Bookworms/app/_layout.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { useEffect } from 'react'
+import { useFont } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  return (
-    <View className="text-2xl">
-      <Text>RootLayout</Text>
-    </View>
-  )
+  
+  const [fontsLoaded, error] = useFont({
+    "Bodoni": require("../assets/fonts/BodoniModa_18pt-Regular.ttf"),
+    "Bodoni-Bold": require("../assets/fonts/BodoniModa_18pt-Bold.ttf"),
+    "Bodoni-Italic": require("../assets/fonts/BodoniModa_18pt-SemiBoldItalic.ttf"),
+    "Inknut": require("../assets/fonts/InknutAntiqua-Regular.ttf"),
+    "Inknut-Light": require("../assets/fonts/InknutAntiqua-Light.ttf"),
+    "Inknut-Bold": require("../assets/fonts/InknutAntiqua-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return(
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }}/>
+      <Stack.Screen name="index" options={{ headerShown: false }}/>
+    </Stack>
+  );
+
 }
 
 export default RootLayout
