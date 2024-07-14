f60fa73914bc355160a3cc150e3945315d0de4b2
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-07-14 16:43:41 -0400

    setup

--- Bookworms/tailwind.config.js
+++ Bookworms/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./app/**/*.{js, jsx}", "./components/**/*.{js, jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      fontFamily: {
+        "bodoni": ["Bodoni", "sans-serif"],
+        "bodoni-bold": ["Bodoni-Bold", "sans-serif"],
+        "bodoni-italic": ["Bodoni-Italic", "sans-serif"],
+        "inknut": ["Inknut", "sans-serif"],
+        "inknut-thin": ["Inknut-Light", "sans-serif"],
+        "inknut-bold": ["Inknut-Bold", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
