a5d33ae9274de88af3cec97bff24ac02dd76916f
renee-k <nahleekhan@gmail.com>
2024-07-17 23:58:56 -0400

    added supabase file

---
+++ Bookworms/utils/supabase.js
@@ -0,0 +1,16 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+// safe to be exposed because of Supabase's security
+const supabaseUrl = "https://rcufxvsfblvrxhcvptvd.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjdWZ4dnNmYmx2cnhoY3ZwdHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExNzY4MjMsImV4cCI6MjAzNjc1MjgyM30.wF4NDMlupk-tZD7--UwGfXpvt6VeuBk4F-L-_SivdvA";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
