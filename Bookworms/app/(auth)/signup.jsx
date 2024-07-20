e55d8ed24cfc9257dec31fdd230e41c18d9f86e0
renee-k <nahleekhan@gmail.com>
2024-07-20 00:02:54 -0400

    started supabase auth in login and sign up page

--- Bookworms/app/(auth)/signup.jsx
+++ Bookworms/app/(auth)/signup.jsx
@@ -1,5 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, AppState, Alert } from 'react-native'
+import { supabase } from '../../lib/supabase'
 import React from 'react'
+
+// Supabase Auth continuously refreshes to check state changes (token refreshed or signed out)
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
 
 const SignUp = () => {
   return (
