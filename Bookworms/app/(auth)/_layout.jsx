1beb723f95ad67bc461127af48f3a4fdb95e052f
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-08-04 11:55:24 -0400

    Added stack to layout

--- Bookworms/app/(auth)/_layout.jsx
+++ Bookworms/app/(auth)/_layout.jsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Redirect, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Loader } from "../../../Bookworms/components/Loader";
+import React from "react";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Stack>
+        <Stack.Screen name="login" options={{ headerShown: false }}/>
+        <Stack.Screen name="signup" options={{ headerShown: false }}/>
+      </Stack>
 
-export default AuthLayout
+      {/* Set isLoading to false for now. Later, when we have an actual variable to check if its loading, we'll use that. */}
+      {/* <Loader isLoading={false}/>  */}
+      <StatusBar backgroundColor="#B2906B" style="dark"/>
+    </>
+  );
+};
+
+export default AuthLayout;
