d511c96ea93fa5c86485b7ca98da1b302388c54c
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-08-05 17:40:20 -0400

    Added icons

---
+++ Bookworms/constants/icons.js
@@ -0,0 +1,11 @@
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+import search from "../assets/icons/search.png";
+import message from "../assets/icons/message.png";
+
+export default {
+  eyeHide,
+  eye,
+  search,
+  message,
+};
