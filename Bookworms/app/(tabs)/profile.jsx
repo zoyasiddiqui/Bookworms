05e1699897cb01309c9e4083335f2bd64dd6fbd2
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-08-11 20:51:24 -0400

    Added profile elements

--- Bookworms/app/(tabs)/profile.jsx
+++ Bookworms/app/(tabs)/profile.jsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { Redirect, router } from "expo-router";
 import { View, Text, Image, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NativeWindStyleSheet } from "nativewind";
 import { Avatar } from 'react-native-elements';
 import OpenButton from "../../../Bookworms/components/OpenButton";
+import Header from "../../../Bookworms/components/Header";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -19,53 +19,62 @@ const Profile = () => {
         {/* This will contain all of the elements */}
         <View className="px-4 items-center flex-1 justify-between">
           {/* This view will contain the title and the logo */}
-          <View className="flex-row justify-center items-centers flex-1">
-            
-            <Avatar 
-              rounded={true}
-              size={64}
-              title="MD"
-              showEditButton={true}
-              containerStyle={styles.avatarContainer}
+          <View className="flex-col justify-center items-center flex-1">
+
+            <Header
+              title="Name"
+              font="font-bodoni"
+              size="text-xl"
+              padding="pt-4 pb-2"
+              margin="mt-10"
             />
 
-            {/* This view will contain all the text/buttons */}
-            <View className="flex-col items-center justify-beg">
-              {/* This view will contain all the top text */}
-              <View className="flex-row justify-center items-center">
+            <View className="flex-row justify-center items-centers flex-1">
+              
+              <Avatar 
+                rounded={true}
+                size={64}
+                title="MD"
+                showEditButton={true}
+                containerStyle={styles.avatarContainer}
+              />
 
-                <View className="flex-col justify-center items-center">
-                  <Text className="font-inknutbold text-sm pt-2">45</Text>
-                  <Text className="font-inknutthin text-sm px-2 pt-2">Followers</Text>
-                </View>
-                <View className="flex-col justify-center items-center">
-                  <Text className="font-inknutbold text-sm pt-2">47</Text>
-                  <Text className="font-inknutthin text-sm px-2 pt-2">Following</Text>
+              {/* This view will contain all the text/buttons */}
+              <View className="flex-col items-center justify-beg">
+                {/* This view will contain all the top text */}
+                <View className="flex-row justify-center items-center">
+
+                  <View className="flex-col justify-center items-center">
+                    <Text className="font-inknutbold text-sm pt-2">45</Text>
+                    <Text className="font-inknutthin text-sm px-2 pt-2">Followers</Text>
+                  </View>
+                  <View className="flex-col justify-center items-center">
+                    <Text className="font-inknutbold text-sm pt-2">47</Text>
+                    <Text className="font-inknutthin text-sm px-2 pt-2">Following</Text>
+                  </View>
+
+                  <OpenButton title={"Reader"}
+                    buttonSize={"w-20 h-10 mx-2"}
+                    buttonColor={"bg-plight rounded-full"}
+                    buttonPadding={"py-6"}
+                    textSize={"text-sm"}
+                    textColor={"text-accentdark"}
+                    shadow={true}
+                  />
                 </View>
 
-                <OpenButton title={"Reader"}
-                  buttonSize={"w-20 h-10 mx-2"}
-                  buttonColor={"bg-plight rounded-full"}
-                  buttonPadding={"py-6"}
+                <OpenButton title={ "Edit Profile" }
+                  buttonSize={"w-40 h-10"}
+                  buttonColor={"bg-slight"}
+                  buttonPadding={""}
                   textSize={"text-sm"}
                   textColor={"text-accentdark"}
-                  shadow={true}
                 />
               </View>
-
-              <OpenButton title={ "Edit Profile" }
-                handlePress={() => login()}
-                buttonSize={"w-40 h-10"}
-                buttonColor={"bg-slight"}
-                buttonPadding={""}
-                textSize={"text-sm"}
-                textColor={"text-accentdark"}
-              />
             </View>
           </View>
-
         </View>
-       
+
       </ScrollView>
     </SafeAreaView>
   )
