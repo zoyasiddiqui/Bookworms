66423126cd2f879af53b220ecdc843d121637ee2
renee-k <nahleekhan@gmail.com>
2024-08-24 00:56:37 -0400

    added functionality to send a photo to supabase

--- Bookworms/app/(tabs)/profile.jsx
+++ Bookworms/app/(tabs)/profile.jsx
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image, Alert } from "react-native";
+import { Avatar } from 'react-native-elements';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NativeWindStyleSheet } from "nativewind";
-import { Avatar } from 'react-native-elements';
 import { useState } from 'react'
 import { supabase } from '../../lib/supabase'
 import OpenButton from "../../../Bookworms/components/OpenButton";
@@ -15,6 +16,8 @@ NativeWindStyleSheet.setOutput({
 });
 
 const Profile = () => {
+  // during profile pic uploads
+  const [uploading, setUploading] = useState(false)
 
   // setting up user info
   const [curUser, setCurUser] = useState({
@@ -56,9 +59,101 @@ const Profile = () => {
       tag: 'Reader', // You might want to get this from the database as well if it's dynamic
       avatar: profile.avatar_url || null, // Assuming your profile has an avatar_url field
     });
-
   }
 
+  // upload the image to supabase
+  async function uploadAvatar(size = 80) {
+    try {
+      setUploading(true)
+
+      // maybe should check/ask user's permissions first?
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images, // Restrict to only images
+        allowsMultipleSelection: false, // Can only select one image
+        aspect: [size, size],
+        allowsEditing: true, // Allows the user to crop / rotate their photo before uploading it
+        quality: 1, // of compression
+        exif: false, // don't need EXIF data
+      })
+
+      if (result.canceled || !result.assets || result.assets.length === 0) {
+        console.log('User cancelled image picker.')
+        return // set uploading false b4?
+      }
+
+      const image = result.assets[0]
+      console.log('Got image', image)
+
+      if (!image.uri) {
+        throw new Error('No image uri!') // this should never happen, just in case
+      }
+
+      const arraybuffer = await fetch(image.uri).then((res) => res.arrayBuffer())
+
+      const fileExt = image.uri?.split('.').pop()?.toLowerCase() ?? 'jpeg' // get file extension
+      const path = `${Date.now()}.${fileExt}` // so file paths are unique
+
+      // send to Supabase storage
+      const { data, error: uploadError } = await supabase.storage
+        .from('avatars')
+        .upload(path, arraybuffer, {
+          contentType: image.mimeType ?? 'image/jpeg',
+        })
+
+      if (uploadError) {
+        throw uploadError
+      }
+
+      updateProfile(data.path)
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message)
+      } else {
+        throw error
+      }
+    } finally {
+      setUploading(false)
+    }
+  }
+
+  // update profile table (maybe we might want this in profile.jsx instead)
+  async function updateProfile(avatar_url = curUser.avatar, first_name = curUser.firstName, last_name = curUser.lastName) {
+    console.log(avatar_url)
+    try {
+      const { data : {user}, error: userRetrievalError, } = await supabase.auth.getUser()
+
+      if (userRetrievalError){
+        Alert.alert(userRetrievalError.message)
+        return;
+      } 
+
+      const updates = {
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        avatar_url: avatar_url
+      }
+
+      const { error } = await supabase.from('profiles').upsert(updates)
+
+      // updating state with new profile picture
+      setCurUser({
+        firstName: user.first_name || 'First',
+        lastName: user.last_name || 'Last',
+        tag: 'Reader', // You might want to get this from the database as well if it's dynamic
+        avatar: avatar_url, // Assuming your profile has an avatar_url field
+      });
+      console.log(curUser)
+
+      if (error) {
+        throw error
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message)
+      }
+    }
+  }
 
   return (
     <SafeAreaView className="bg-bglight h-full flex">
@@ -89,11 +184,13 @@ const Profile = () => {
             {/* This will hold the profile picture, the followers and following stuff, and the tag */}
             <View className="flex-row justify-center items-center">
 
-              <Avatar size={80}
+            <Avatar size={80}
                 title={`${curUser.firstName[0]} ${curUser.lastName[0]}`}
                 showEditButton={true}
                 containerStyle={styles.avatarContainer}
-              />
+                source={`${curUser.avatar_url ? curUser.avatar_url : require('../../assets/images/profile.png')}`}
+                onPress={() => uploadAvatar()}
+            />
 
               <View className="flex-row justify-center items-center">
                 <View className="flex-col justify-center items-center">
@@ -191,6 +288,6 @@ const styles = StyleSheet.create({
     padding: 2,            // Optional: Adds some padding inside the border
     margin: 10,
   },
-})
+})  
 
 export default Profile
