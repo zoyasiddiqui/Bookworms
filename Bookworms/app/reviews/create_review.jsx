6d684f90424525def3071bd13249ba1f09250cb2
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-08-27 21:08:27 -0400

    Start on the create reviews page

--- Bookworms/app/reviews/create_review.jsx
+++ Bookworms/app/reviews/create_review.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import CreateReview from '../../components/reviews/CreateReview'
+import { StatusBar } from 'expo-status-bar'
 
 const create_review = () => {
   return (
-    <CreateReview/>
+    <SafeAreaView className="bg-bglight h-full flex">
+        <StatusBar/>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+
+            <CreateReview/>
+
+        </ScrollView>
+    </SafeAreaView>
   )
 }
 
