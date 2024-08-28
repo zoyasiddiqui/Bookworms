ddc48455cbb414ec7f4305688ef8e12d88938b66
renee-k <nahleekhan@gmail.com>
2024-08-27 23:14:51 -0400

    added quote component files

---
+++ Bookworms/components/quotes/Quote.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Quote() {
+  return (
+    <View>
+      <Text>Quote</Text>
+    </View>
+  )
+}
---
+++ Bookworms/components/quotes/QuotesView.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function QuotesView() {
+  return (
+    <View>
+      <Text>QuotesView</Text>
+    </View>
+  )
+}
